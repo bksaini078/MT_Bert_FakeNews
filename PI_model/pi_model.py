369286b46fe1ae67383e24893f86c225c4f1f32c
Bhupender Kumar Saini <bksaini078@gmail.com>
2021-01-02 13:38:55 +0100

    included CNN in Pi model

---
+++ PI_model/pi_model.py
@@ -0,0 +1,215 @@
+import math
+import numpy as np 
+import tensorflow as tf
+from  tensorflow.keras.layers  import *
+
+# Edited files with weight normalization and mean only batch normalization
+from PI_model.weight_norm_layers.Conv2D import Conv2D
+from PI_model.weight_norm_layers.Dense import Dense
+
+def create_embedding(x):
+    # x = tf.make_ndarray(x)
+    x=tf.expand_dims(x, -1)
+    # x=tf.reshape(x, (x.shape)+ [1], name=None)
+    # tf.convert_to_tensor(x )
+    return x
+
+class PiModel(tf.keras.Model):
+    """ Class for defining eager compatible tfrecords file
+
+        I did not use tfe.Network since it will be depracated in the
+        future by tensorflow.
+    """
+
+    def __init__(self,args, vocab_size):
+        """ Init
+
+            Set all the layers that need to be tracked in the process of
+            gradients descent (pooling and dropout for example dont need
+            to be stored)
+        """
+        self.max_len = args.max_len
+        self.vocab_size= vocab_size
+
+        super(PiModel, self).__init__()
+        self._emb= Embedding(vocab_size, 128, input_length=None)
+
+        self._conv1a = Conv2D.Conv2D ( filters=128, kernel_size=[3, 3],
+                                       padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                       kernel_initializer=tf.keras.initializers.he_uniform (),
+                                       bias_initializer=tf.keras.initializers.constant (
+                                           0.1 ),
+                                       weight_norm=True, mean_only_batch_norm=True )
+        self._conv1b = Conv2D.Conv2D ( filters=128, kernel_size=[3, 3],
+                                       padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                       kernel_initializer=tf.keras.initializers.he_uniform (),
+                                       bias_initializer=tf.keras.initializers.constant (
+                                           0.1 ),
+                                       weight_norm=True, mean_only_batch_norm=True )
+        self._conv1c = Conv2D.Conv2D ( filters=128, kernel_size=[3, 3],
+                                       padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                       kernel_initializer=tf.keras.initializers.he_uniform (),
+                                       bias_initializer=tf.keras.initializers.constant (
+                                           0.1 ),
+                                       weight_norm=True, mean_only_batch_norm=True )
+        self._pool1 = tf.keras.layers.MaxPool2D (
+            pool_size=2, strides=2, padding="same" )
+        self._dropout1 = tf.keras.layers.Dropout ( 0.5 )
+
+        self._conv2a = Conv2D.Conv2D ( filters=256, kernel_size=[3, 3],
+                                       padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                       kernel_initializer=tf.keras.initializers.he_uniform (),
+                                       bias_initializer=tf.keras.initializers.constant (
+                                           0.1 ),
+                                       weight_norm=True, mean_only_batch_norm=True )
+        self._conv2b = Conv2D.Conv2D ( filters=256, kernel_size=[3, 3],
+                                       padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                       kernel_initializer=tf.keras.initializers.he_uniform (),
+                                       bias_initializer=tf.keras.initializers.constant (
+                                           0.1 ),
+                                       weight_norm=True, mean_only_batch_norm=True )
+        self._conv2c = Conv2D.Conv2D ( filters=256, kernel_size=[3, 3],
+                                       padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                       kernel_initializer=tf.keras.initializers.he_uniform (),
+                                       bias_initializer=tf.keras.initializers.constant (
+                                           0.1 ),
+                                       weight_norm=True, mean_only_batch_norm=True )
+        self._pool2 = tf.keras.layers.MaxPool2D (
+            pool_size=2, strides=2, padding="same" )
+        self._dropout2 = tf.keras.layers.Dropout ( 0.5 )
+
+        self._conv3a_sup = Conv2D.Conv2D ( filters=512, kernel_size=[3, 3],
+                                           padding="valid", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                           kernel_initializer=tf.keras.initializers.he_uniform (),
+                                           bias_initializer=tf.keras.initializers.constant (
+                                               0.1 ),
+                                           weight_norm=True, mean_only_batch_norm=True )
+        self._conv3b_sup = Conv2D.Conv2D ( filters=256, kernel_size=[1, 1],
+                                           padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                           kernel_initializer=tf.keras.initializers.he_uniform (),
+                                           bias_initializer=tf.keras.initializers.constant (
+                                               0.1 ),
+                                           weight_norm=True, mean_only_batch_norm=True )
+        self._conv3c_sup = Conv2D.Conv2D ( filters=128, kernel_size=[1, 1],
+                                           padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                           kernel_initializer=tf.keras.initializers.he_uniform (),
+                                           bias_initializer=tf.keras.initializers.constant (
+                                               0.1 ),
+                                           weight_norm=True, mean_only_batch_norm=True )
+
+        self._conv3a_unsup = Conv2D.Conv2D ( filters=512, kernel_size=[3, 3],
+                                             padding="valid", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                             kernel_initializer=tf.keras.initializers.he_uniform (),
+                                             bias_initializer=tf.keras.initializers.constant (
+                                                 0.1 ),
+                                             weight_norm=True, mean_only_batch_norm=True )
+        self._conv3b_unsup = Conv2D.Conv2D ( filters=256, kernel_size=[1, 1],
+                                             padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                             kernel_initializer=tf.keras.initializers.he_uniform (),
+                                             bias_initializer=tf.keras.initializers.constant (
+                                                 0.1 ),
+                                             weight_norm=True, mean_only_batch_norm=True )
+        self._conv3c_unsup = Conv2D.Conv2D ( filters=128, kernel_size=[1, 1],
+                                             padding="same", activation=tf.keras.layers.LeakyReLU ( alpha=0.1 ),
+                                             kernel_initializer=tf.keras.initializers.he_uniform (),
+                                             bias_initializer=tf.keras.initializers.constant (
+                                                 0.1 ),
+                                             weight_norm=True, mean_only_batch_norm=True )
+
+        self._dense_sup = Dense.Dense ( units=2, activation=tf.nn.softmax,
+                                        kernel_initializer=tf.keras.initializers.he_uniform (),
+                                        bias_initializer=tf.keras.initializers.constant (
+                                            0.1 ),
+                                        weight_norm=True, mean_only_batch_norm=True )
+        self._dense_unsup = Dense.Dense ( units=2, activation=tf.nn.softmax,
+                                          kernel_initializer=tf.keras.initializers.he_uniform (),
+                                          bias_initializer=tf.keras.initializers.constant (
+                                              0.1 ),
+                                          weight_norm=True, mean_only_batch_norm=True )
+
+    def __aditive_gaussian_noise(self, input, std):
+        """ Function to add additive zero mean noise as described in the paper
+
+        Arguments:
+            input {tensor} -- image
+            std {int} -- std to use in the random_normal
+
+        Returns:
+            {tensor} -- image with added noise
+        """
+
+        noise = tf.random.normal(shape=tf.shape(
+            input), mean=0.0, stddev=std, dtype=tf.float32)
+        return input + noise
+
+    def __apply_image_augmentation(self, image):
+        """ Applies random transformation to the image (shift image withina range of 
+            [-2, 2] pixels)
+
+        Arguments:
+            image {tensor} -- image
+
+        Returns:
+            {tensor} -- transformed image
+        """
+
+        random_shifts = np.random.randint(-2, 2, (image.numpy().shape[0], 2))
+        random_transformations = tf.contrib.image.translations_to_projective_transforms(
+            random_shifts)
+        image = tf.contrib.image.transform(image, random_transformations, 'NEAREST',
+                                           output_shape=tf.convert_to_tensor(image.numpy().shape[1:3], dtype=np.int32))
+        return image
+
+    def call(self, input, training=True):
+        """ Function that allows running a tensor through the pi model
+
+        Arguments:
+            input {[tensor]} -- batch of images
+            training {bool} -- if true applies augmentaton and additive noise
+
+        Returns:
+            [tensor] -- predictions"""
+
+ 
+
+        # if training:
+        #     h = self.__aditive_gaussian_noise(input, 0.15)
+        #     h = self.__apply_image_augmentation(h)
+        # else:
+        h = input
+        h=self._emb(h)
+        
+        h= create_embedding(h)
+
+        h1a = self._conv1a ( h, training )
+        h1b = self._conv1b ( h1a, training )
+        h1c = self._conv1c ( h1b, training )
+        h1p = self._pool1 ( h1c )
+        h1d = self._dropout1 ( h1p, training=training )
+
+        h2a = self._conv2a ( h1d, training )
+        h2b = self._conv2b ( h2a, training )
+        h2c = self._conv2c ( h2b, training )
+        h2p = self._pool2 ( h2c )
+        h2d = self._dropout2 ( h2p, training=training )
+
+        h3a_sup = self._conv3a_sup ( h2d, training )
+        h3b_sup = self._conv3b_sup ( h3a_sup, training )
+        h3c_sup = self._conv3c_sup ( h3b_sup, training )
+
+        # Supervised Average Pooling
+        # hm_sup = tf.reduce_mean(h3c_sup, reduction_indices=[1, 2])
+        # dense_sup = self._dense(hm_sup, training)
+
+        h3a_unsup = self._conv3a_unsup ( h2d, training )
+        h3b_unsup = self._conv3b_unsup ( h3a_unsup, training )
+        h3c_unsup = self._conv3c_unsup ( h3b_unsup, training )
+
+        # Unsupervised Average Pooling
+        hm_unsup = tf.reduce_mean ( h3c_unsup, reduction_indices=[1, 2] )
+        dense_unsup = self._dense_sup ( hm_unsup, training )
+
+        # Supervised Average Pooling
+        hm_sup = tf.reduce_mean ( h3c_sup, reduction_indices=[1, 2] )
+        dense_sup = self._dense_unsup ( hm_sup, training )
+        return dense_sup, dense_unsup
--- PI_model/train_pi_model.py
+++ PI_model/train_pi_model.py
@@ -1,76 +1,79 @@
 import tensorflow as tf
 import numpy as np
-
-# this is to enable eager execution
-tf.compat.v1.enable_eager_execution ()
+#this is to enable eager execution
+tf.compat.v1.enable_eager_execution()
 from Mean_Teacher.report_writing import report_writing
 from Mean_Teacher.data_loader import data_slices
+
 from Mean_Teacher.evaluation import prec_rec_f1score
-from PI_model.pi_costfunction import pi_model_loss, ramp_down_function, ramp_up_function
-from BERT.bert import BERT
+from PI_model.pi_costfunction import pi_model_loss,ramp_down_function,ramp_up_function
+from PI_model.pi_model import PiModel
 
-
-def Pimodel(args, fold, x_train, y_train, x_val, y_val, x_test, y_test, x_unlabel_tar, vocab_size) :
-    x_unlabel_tar = x_unlabel_tar[:len(x_train[0])]
-    NUM_TRAIN_SAMPLES = len(x_train[0]) + len (x_unlabel_tar[0])
-    NUM_TEST_SAMPLES =len(x_test[0])
+def train_Pimodel(args,fold, x_train, y_train, x_val, y_val, x_test, y_test,x_unlabel_tar,vocab_size) :
+    x_unlabel_tar = x_unlabel_tar[:len ( x_train )]
+    NUM_TRAIN_SAMPLES = len(x_train)+len(x_unlabel_tar)
+    NUM_TEST_SAMPLES = np.shape(x_test)[0]
 
     # Editable variables
-    num_labeled_samples = int (len(x_train[0]))
-    num_validation_samples =  len(x_val[0])
-    max_learning_rate = args.lr
+    num_labeled_samples = int(len(x_train))
+    num_validation_samples = np.shape(x_val )[0]
+    max_learning_rate = 0.003
     initial_beta1 = 0.9
     final_beta1 = 0.5
 
+
     learning_rate = tf.Variable(max_learning_rate)  # max learning rate
     beta_1 = tf.Variable ( initial_beta1 )
-    # optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta_1, beta_2=0.999 )
-    optimizer = tf.keras.optimizers.Adam( learning_rate=args.lr)
-    train_dataset = data_slices ( args, x_train, y_train )
-    pi_model = BERT(args).create_model()
-    pi_model.summary ()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta_1, beta_2=0.999 )
+    train_dataset= data_slices(args, x_train,y_train)
+    # preparing the training dataset
+    pi_model= PiModel(args, vocab_size)
+    pi_model.compile ( optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'] )
+
     train_metrics = tf.keras.metrics.BinaryAccuracy ( name='Binary_Accuracy' )
     val_metrics = tf.keras.metrics.BinaryAccuracy ( name='val_Accuracy' )
 
-    max_unsupervised_weight = 1
+    tf.print(num_labeled_samples, NUM_TRAIN_SAMPLES )
+    max_unsupervised_weight = 100 * num_labeled_samples * (NUM_TRAIN_SAMPLES)
+    progbar = tf.keras.utils.Progbar ( len ( train_dataset ), stateful_metrics=['Accuracy', 'Overall_Loss', 'val_acc'] )
 
-    progbar = tf.keras.utils.Progbar ( len ( train_dataset ), stateful_metrics=['Accuracy', 'Overall_Loss','val_acc'] )
+    for epoch in range( 1, args.epochs + 1 ) :
+        tf.print ( 'epoch %d' % (epoch,) )
 
-    for epoch in range (args.epochs) :
-        tf.print ( 'epoch: %d' % (epoch,) )
-        # rampdown_value = ramp_down_function(epoch, args.epochs)
+        rampdown_value = ramp_down_function(epoch, args.epochs)
         rampup_value = ramp_up_function(epoch)
         if epoch == 0 :
             unsupervised_weight = 0
         else :
             unsupervised_weight = max_unsupervised_weight * rampup_value
-        # learning_rate.assign(rampup_value * rampdown_value * max_learning_rate )
-        # print(f'Learning rate: {learning_rate}')
-        # beta_1.assign(rampdown_value * initial_beta1 + (1.0 - rampdown_value) * final_beta1 )
-        for step, (inputs, attention, token_id, y_batch_train) in enumerate ( train_dataset ) :
+
+        learning_rate.assign(rampup_value * rampdown_value * max_learning_rate )
+        tf.print(f'Learning rate: {learning_rate}')
+        beta_1.assign(rampdown_value * initial_beta1 + (1.0 - rampdown_value) * final_beta1 )
+
+        for step, (x_batch_train, y_batch_train) in enumerate( train_dataset ):
             with tf.GradientTape () as tape :
-                loss_value = pi_model_loss ( args, [inputs, attention, token_id], y_batch_train, x_unlabel_tar,
-                                             pi_model, unsupervised_weight )
-            grads = tape.gradient ( loss_value, pi_model.variables )
-            optimizer.apply_gradients (
-                (grad, var) for (grad, var) in zip ( grads, pi_model.variables ) if grad is not None )
-            logits = pi_model ( [inputs, attention, token_id], training=True )
+                # x_batch_unlabel = iterator_unlabel.get_next()
+                loss_value,grads = pi_model_loss( x_batch_train, y_batch_train, x_unlabel_tar, pi_model,unsupervised_weight )
+            optimizer.apply_gradients((grad, var) for (grad, var) in zip ( grads, pi_model.variables ) if grad is not None )
+            # Run the forward pass of the layer
+            logits = pi_model (x_batch_train, training=True )
             train_acc = train_metrics ( tf.argmax ( y_batch_train, 1 ), tf.argmax ( logits, 1 ) )
+            loss = tf.keras.losses.categorical_crossentropy(y_batch_train, logits)
             progbar.add ( args.batch_size, values=[('Accuracy', train_acc), ('Overall_Loss', loss_value)] )
-            p = np.random.permutation(args.batch_size+6)
-            x_val_t= [x_val[0][p],x_val[1][p],x_val[2][p]]
-            y_val_t= y_val[p]
-            y_v_p = pi_model(x_val_t)
+            p = np.random.permutation ( args.batch_size)
+            x_val_t= x_val[p]
+            y_val_t=y_val[p]
+            y_v_p= pi_model(x_val_t)
             val_acc = val_metrics ( tf.argmax ( y_val_t, 1 ), tf.argmax ( y_v_p.numpy (), 1 ) )
-            progbar.update( step, values=[('val_acc', val_acc)] )
+            progbar.update ( step, values=[('val_acc', val_acc)] )
+
 
     print ( '---------------------------Pi Model TEST--------------------------' )
-    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, AUC = prec_rec_f1score (
-        args, y_test, x_test, pi_model )
-    report_writing ( args, fold, args.model + '_' + 'BERT_'+args.pretrained_model, args.lr, args.batch_size, args.epochs, args.alpha,
-                     args.ratio, train_acc.numpy (), test_accuracy,
-                     precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, AUC )
-    # pi_model.save (f'{args.model_output_folder}/{args.data}/{args.model}_BERT_{args.alpha}_{args.pretrained_model}_fold-{fold}')
+    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, AUC = prec_rec_f1score (args,y_test, x_test, pi_model )
+    report_writing(args,fold,args.model+'_', args.lr, args.batch_size, args.epochs, args.alpha, args.ratio, train_acc.numpy(),test_accuracy,
+    precision_true, precision_fake, recall_true, recall_fake,f1score_true, f1score_fake, AUC)
+    # pi_model.save (f'{args.model_output_folder}/{args.data}/{args.model}_fold-{fold}')
     print ( '-----------------------------------------------------------------' )
     tf.keras.backend.clear_session ()
     return pi_model
