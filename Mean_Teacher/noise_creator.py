9f207f19e55fccac03ec554ebdc939e9a6ad327d
isspek <ipekbrs@gmail.com>
2021-01-22 14:56:01 +0100

    code reviewing

--- Mean_Teacher/noise_creator.py
+++ Mean_Teacher/noise_creator.py
@@ -12,20 +12,20 @@ def unison_shuffled(x1,x2,y, args):
 def instant_noise_bert(x_train,y_train,x_unlabel, args):
     '''Adding unlabel data to train data'''
     # need to include noise parameter to control to maintain unlabel ratio
-    indices = tf.range ( start=0, limit=len ( x_unlabel[0] ), dtype=tf.int32 )
-    shuffled_indices = tf.random.shuffle ( indices )
+    indices = tf.range(start=0, limit=len(x_unlabel[0]), dtype=tf.int32)
+    shuffled_indices = tf.random.shuffle(indices)
 
-    shuffled_inp = tf.gather ( x_unlabel[0], shuffled_indices )
-    shuffled_attn = tf.gather ( x_unlabel[1], shuffled_indices )
+    shuffled_inp = tf.gather(x_unlabel[0], shuffled_indices)
+    shuffled_attn = tf.gather(x_unlabel[1], shuffled_indices)
 
-    y_train_n = np.full((len(x_unlabel[0]),2), -1)
-    input_id= np.append(x_train[0],x_unlabel[0],axis=0)
-    atten_id= np.append(x_train[1],x_unlabel[1],axis=0)
-    y = np.append(y_train, y_train_n,axis=0)
+    y_train_n = np.full((len(x_unlabel[0]), 2), -1)
+    input_id = np.append(x_train[0], shuffled_inp, axis=0)
+    atten_id = np.append(x_train[1], shuffled_attn, axis=0)
+    y = np.append(y_train, y_train_n, axis=0)
 
-    #not performing random permutation, effecting the result
+    # not performing random permutation, effecting the result
     # p = np.random.permutation(len(x0))[:len(x0)-2]
-    
+
     # now unison permutation
-    return [input_id,atten_id],y #unison_shuffled(x0,x1,y,args)
+    return [input_id, atten_id], y  # unison_shuffled(x0,x1,y,args)
 
--- Mean_Teacher/train_MeanTeacher.py
+++ Mean_Teacher/train_MeanTeacher.py
@@ -5,63 +5,67 @@ from Mean_Teacher.costfunction import Overall_Cost, EMA
 from Mean_Teacher.report_writing import report_writing
 from Mean_Teacher.evaluation import prec_rec_f1score
 from Mean_Teacher.data_loader import data_slices
-from BERT.bert import BERT
-import numpy as np
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_recall_fscore_support
+# from BERT.bert import BERT
+from src.clf.bert import BERT
 
 
-def MeanTeacher(args, fold, x_train, y_train,  x_test, y_test, x_unlabel_tar) :
+def MeanTeacher(args, fold, x_train, y_train, x_test, y_test, x_unlabel_tar):
     # preparing the training dataset
-    train_dataset, unlabel_dataset = data_slices(args, x_train, y_train,x_unlabel_tar)
-    
+    train_dataset, unlabel_dataset = data_slices(args, x_train, y_train, x_unlabel_tar)
+
     # declaring optimiser
-    optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr) 
-    
-    #creating model
-    student = BERT(args).create_model()
-    teacher = BERT(args).create_model()
-    
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
+
+    # Ipek- I changed with the BERT under the src/clf teacher model should not be fine tuned during the training, otherwise it will not be mean teacher.
+    # This makes fake news results worse, but we should do like this based on original paper
+    # creating model
+    student = BERT(args).create_model(training=True)
+    teacher = BERT(args).create_model(training=False)
+
     # declaring metrics
     train_metrics = tf.keras.metrics.BinaryAccuracy(name='Binary_Accuracy')
-    
+
     progbar = tf.keras.utils.Progbar(len(train_dataset), stateful_metrics=['Accuracy', 'Overall_Loss'])
-    
-    #training
-    for epoch in range(1, args.epochs + 1) :
-        tf.print('\nepoch %d' %(epoch,))
-        for step,(inputs, attention,  y_batch_train) in enumerate(train_dataset):
-            with tf.GradientTape() as tape :
+
+    # TODO: There is a problem here, it does not loop 3 epochs, it only loop once.
+    for epoch in range(args.epochs):
+        tf.print(f'\nepoch {epoch + 1}')
+        for step, (inputs, attention, y_batch_train) in enumerate(train_dataset):
+            with tf.GradientTape() as tape:
+                # TODO please rename unlabel as noise, it is confusing
+                # QUESTION: Here I couldn't understand the logic of reading noise data, you only read one batch by doing iter and next. Are we sure that we get user defined batch size inputs?
                 iterator_unlabel = iter(unlabel_dataset)
                 x_batch_unlabel = iterator_unlabel.get_next()
+
+                # TODO Please split to overall cost of sub methods, here it is very confusing. Do how I did 1) agumentation, 2) student cost 3) augmentation and then overall cost
                 overall_cost = Overall_Cost(args, [inputs, attention], y_batch_train, x_batch_unlabel,
-                                              student, teacher)
-              
+                                            student, teacher)
+
             grads = tape.gradient(overall_cost, student.trainable_weights)
-            optimizer.apply_gradients((grad, var) for(grad, var) in zip(grads, student.trainable_weights)if grad is not None)
+            optimizer.apply_gradients(
+                (grad, var) for (grad, var) in zip(grads, student.trainable_weights) if grad is not None)
 
             # applying student weights to teacher
             teacher = EMA(student, teacher, alpha=args.alpha)
 
-            #calculating training accuracy
+            # calculating training accuracy
             logits_t = teacher([inputs, attention])
             train_acc = train_metrics(tf.argmax(y_batch_train, 1), tf.argmax(logits_t, 1))
-            progbar.update(step, values=[('Accuracy', train_acc),('Overall_Loss', overall_cost)])
 
+            # TODO: Overall cost gives always NaN
+            progbar.update(step, values=[('Accuracy', train_acc), ('Overall_Loss', overall_cost)])
 
-    #Evaluation of the model
+    # Evaluation of the model
     tf.print("\nStudent Testing data evaluation:")
-    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, f1_macro,f1_micro,f1_weighted,AUC = prec_rec_f1score(
+    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, f1_macro, f1_micro, f1_weighted, AUC = prec_rec_f1score(
         args, y_test, x_test, student)
-    
+
     tf.print("\nTeacher Testing data evaluation:")
-    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, f1_macro,f1_micro,f1_weighted,AUC = prec_rec_f1score(
+    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, f1_macro, f1_micro, f1_weighted, AUC = prec_rec_f1score(
         args, y_test, x_test, teacher)
     # teacher.save(f'{args.model_output_folder}/{args.data}/{args.model}_BERT_{args.alpha}_{args.pretrained_model}_fold-{fold}')
-    
+
     # reporting in report file 
-    report_writing(args,  train_acc.numpy(), test_accuracy,precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake,f1_macro,f1_micro,f1_weighted, AUC)
+    report_writing(args, train_acc.numpy(), test_accuracy, precision_true, precision_fake, recall_true, recall_fake,
+                   f1score_true, f1score_fake, f1_macro, f1_micro, f1_weighted, AUC)
     tf.keras.backend.clear_session()
-    return
-
-
