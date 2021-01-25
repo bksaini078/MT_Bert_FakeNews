26a7bf5fe746db09decff938325e638e47ab090f
bksaini078 <bksaini078@gmail.com>
2021-01-25 10:15:11 +0000

    included attention drop noise

--- Mean_Teacher/augmentation.py
+++ Mean_Teacher/augmentation.py
@@ -19,22 +19,33 @@ def augment_data(x_train, y_train, x_unlabel, args) :
 
     # shuffled_inp = tf.gather(x_unlabel[0], shuffled_indices)
     # shuffled_attn = tf.gather(x_unlabel[1], shuffled_indices)
+    noised_attention= attention_noise(x_unlabel[1],args)
 
     y_train_n = np.full ( (len ( x_unlabel[0] ), 2), -1 )
     input_id = np.append ( x_train[0], x_unlabel[0], axis=0 )
-    atten_id = np.append ( x_train[1], x_unlabel[1], axis=0 )
+    atten_id = np.append ( x_train[1], noised_attention, axis=0 )
     y = np.append ( y_train, y_train_n, axis=0 )
+    # print(atten_id)
 
-    # introduction attention noise
-    atten_noise = attention_noise ( atten_id, 0.2, args )
+    # introduction attention noise :NOT having good performance
+    # atten_noise = attention_noise ( atten_id, args )
 
     # now unison permutation
-    return [input_id, atten_noise], y  # unison_shuffled(x0,x1,y,args)
+    return [input_id, atten_id], y  # unison_shuffled(x0,x1,y,args)
 
 
-def attention_noise(attention, noise_prob, args) :
-    condition = np.random.choice ( a=[False, True], size=attention.shape, p=[1 - noise_prob, noise_prob] )
-    case_true = 1 - attention  # if 1 then 0 if 0 then 1
-    case_false = attention
-    noised_attention = tf.where ( condition, case_true, case_false )
+def attention_noise(attention, args) :
+    noised_attention= np.zeros(attention.shape)
+    if args.attn_noise==0:
+        return attention
+    for i in range(len(attention)):
+        if np.random.binomial(1,0.3):# tossing to select particular article will be noised or not
+            condition = np.random.choice(a=[False, True], size=attention[i].shape, p=[1 - args.attn_noise, args.attn_noise] )
+            case_true = 1 - attention[i]  # if 1 then 0 if 0 then 1
+            case_false = attention[i]
+            noised_attention[i] = tf.where(condition, case_true, case_false)
+        else:
+            noised_attention[i]=attention[i]
+    noised_attention=tf.convert_to_tensor(noised_attention)
+    # print(noised_attention)
     return noised_attention
--- Mean_Teacher/data_loader.py
+++ Mean_Teacher/data_loader.py
@@ -6,12 +6,12 @@ import numpy as np
 from reader import mask_labels
 
 
-def data_load(args, fold, path) :
-    # will change after some time
+def data_load(args,fold, path):
+    #will change after some time
     # path='Data/ExperimentsFolds/fakehealth/'
-    train_data = pd.read_csv ( path + 'train.tsv', sep='\t' )  # ,nrows=40 )
-    train_data = mask_labels ( train_data, args.ratio_label )
-    train_data = train_data.sample ( frac=1, random_state=args.seed ).reset_index ( drop=True )
+    train_data = pd.read_csv ( path + 'train.tsv', sep='\t')#,nrows=40 )
+    train_data= mask_labels(train_data,args.ratio_label)
+    train_data = train_data.sample(frac=1, random_state=args.seed).reset_index(drop=True)
     # tried experimenting equalt labels , TODO: Will be removed in future
     # but true news parameter are decreasing
     # true=train_data[train_data.label=='true']
@@ -21,47 +21,45 @@ def data_load(args, fold, path) :
 
     # print(train_data.groupby('label').count())
 
-    test_data = pd.read_csv ( path + 'test.tsv', sep='\t' )  # ,nrows=30)
-    noise = pd.read_csv ( path + 'noise.tsv', sep='\t' )  # ,nrows=40 )
-
-    tokenizer = AutoTokenizer.from_pretrained ( args.pretrained_model )
+    test_data= pd.read_csv(path+'test.tsv',sep='\t')#,nrows=30)
+    noise = pd.read_csv(path + 'noise.tsv', sep='\t')#,nrows=40 )
+    
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model)
 
     # creating news example, taking vocab size also, incase
-    train_data, vocab_size = create_news_examples ( train_data, args.max_len, tokenizer )
+    train_data,vocab_size = create_news_examples ( train_data, args.max_len, tokenizer)
     x_train, y_train = create_inputs_targets ( train_data )
 
-    test_data, _ = create_news_examples ( test_data, args.max_len, tokenizer )
-    x_test, y_test = create_inputs_targets ( test_data )
+    test_data,_ = create_news_examples(test_data, args.max_len, tokenizer)
+    x_test, y_test = create_inputs_targets(test_data )
 
-    x_noise, _ = create_news_examples ( noise, args.max_len, tokenizer )
+    x_noise,_ = create_news_examples (noise, args.max_len, tokenizer )
     x_noise, _ = create_inputs_targets ( x_noise )
 
-    print ( 'train size:', np.shape ( x_train[0] ) )
-
-    print ( 'test size:', np.shape ( x_test[0] ) )
-
+    print('train size:',np.shape(x_train[0]))
+ 
+    print('test size:', np.shape(x_test[0]))
+   
     return x_train, y_train, x_test, y_test, x_noise
 
-
-def data_slices(args, x_train, y_train, x_noise) :
-    # this PI model will be removed in the future
-    if args.model == 'PI' :
+def data_slices(args, x_train,y_train,x_noise):
+    #this PI model will be removed in the future
+    if args.model=='PI':
         train_dataset = tf.data.Dataset.from_tensor_slices ( (x_train[0], y_train) )
-        train_dataset = train_dataset.shuffle ( buffer_size=1024 ).batch ( args.batch_size )
+        train_dataset = train_dataset.shuffle( buffer_size=1024 ).batch( args.batch_size)
 
-    else :
-        # TODO:here need to include the ratio of amount of noise per batch
-        train_dataset = tf.data.Dataset.from_tensor_slices ( (x_train[0], x_train[1], y_train) ).batch (
-            args.batch_size )
+    else:
+        #TODO:here need to include the ratio of amount of noise per batch
+        train_dataset = tf.data.Dataset.from_tensor_slices((x_train[0], x_train[1], y_train) ).batch(args.batch_size)
 
-        # QUESTION: Here I couldn't understand the logic noise data will always same size with labeled batch
+        #QUESTION: Here I couldn't understand the logic noise data will always same size with labeled batch
         # Also you are not handling noiseed dataset where we will mask the train set, I have done all those stuff see on my code for the reference
-        noise_batch = int ( args.batch_size * args.noise_ratio )
-        print ( noise_batch )
-        noise_dataset = tf.data.Dataset.from_tensor_slices ( (x_noise[0], x_noise[1]) ).batch ( noise_batch )
+        noise_batch = int(args.batch_size * args.noise_ratio)
+        print(noise_batch)
+        noise_dataset= tf.data.Dataset.from_tensor_slices((x_noise[0],x_noise[1])).batch(noise_batch)
 
-        # in case we have less noise data
-        noise_dataset = noise_dataset.repeat ( args.batch_size )
+        #in case we have less noise data
+        noise_dataset=noise_dataset.repeat(args.batch_size)
 
     return train_dataset, noise_dataset
 
